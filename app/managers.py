42c3b7d0ca5fb888b7e8d2be88d982f7c3a1a4f7
Volodymyr Borovets <vborovets0@gmail.com>
2023-04-27 22:54:25 +0300

    replace SELECT id,first,last -> *

--- app/managers.py
+++ app/managers.py
@@ -18,7 +18,7 @@ class ActorManager:
 
     def all(self) -> list[Actor]:
         actors_cursor = self._connection.execute(
-            f"SELECT id, first_name, last_name FROM {self.table_name}"
+            f"SELECT * FROM {self.table_name}"
         )
         return [Actor(*row) for row in actors_cursor]
 
